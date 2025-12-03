ce3db17ac1d41acebdf9318fd8119bc1246ba073
Vladyslav Rudik <salon200390@gmail.com>
2025-12-03 13:55:52 +0200

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50),
+    created_at TIMESTAMP DEFAULT NOW()
+);
 
+CREATE TABLE manga (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    description TEXT
+);
