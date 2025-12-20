b91ecc7b1d50fd7503fca96a20cb924753bb62ec
Vladyslav Rudik <salon200390@gmail.com>
2025-12-21 00:22:04 +0200

    Update mangaService.js

--- backend/services/mangaService.js
+++ backend/services/mangaService.js
@@ -1,17 +1,14 @@
-// backend/services/mangaService.js
-
 const mangaModel = require("../models/mangaModel");
 
-async function getAllManga() {
-    // пока что заглушка под БД
-    return mangaModel.getAll();
+async function getAllMangas() {
+    return mangaModel.getAllMangas();
 }
 
 async function getMangaById(id) {
-    return mangaModel.getById(id);
+    return mangaModel.getMangaById(id);
 }
 
 module.exports = {
-    getAllManga,
+    getAllMangas,
     getMangaById
 };
