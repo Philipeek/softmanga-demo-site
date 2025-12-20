45b1f58c76738ca9c4eda450f256272757b61ec6
Vladyslav Rudik <salon200390@gmail.com>
2025-12-20 04:24:50 +0200

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const healthController = require("../controllers/healthController");
+const demoController = require("../controllers/demoController");
 
 router.get("/health", healthController.healthCheck);
+router.get("/error-demo", demoController.throwDemoError);
 
 module.exports = router;
