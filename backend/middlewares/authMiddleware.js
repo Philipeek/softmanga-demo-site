5ecad694cd85cd897292ee7b53f373a5c2fffa79
Vladyslav Rudik <salon200390@gmail.com>
2025-12-25 18:01:25 +0200

    Create authMiddleware.js

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const ApiError = require("../utils/ApiError");
+const { verifyToken } = require("../utils/jwt");
+
+function requireAuth(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return next(ApiError.badRequest("Authorization token required"));
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return next(ApiError.badRequest("Invalid or expired token"));
+    }
+}
+
+module.exports = requireAuth;
