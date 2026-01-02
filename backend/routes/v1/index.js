bd9b85cbd1e32bb5c565c55930b8436aa7be11dd
Vladyslav Rudik <salon200390@gmail.com>
2026-01-02 22:55:31 +0200

    Update index.js

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -7,6 +7,9 @@ const mangaController = require("../../controllers/mangaController");
 
 const userController = require("../../controllers/userController");
 
+const requireAuth = require("../../middlewares/authMiddleware");
+const requireRole = require("../../middlewares/roleMiddleware");
+
 router.get("/users", userController.listUsers);
 router.get("/users/:id", userController.getUser);
 
@@ -16,5 +19,15 @@ router.get("/error-demo", demoController.throwDemoError);
 
 router.get("/mangas", mangaController.listMangas);
 router.get("/mangas/:id", mangaController.getManga);
+
+router.get("/admin/panel", requireAuth, requireRole("admin"), (req, res) => {
+    res.json({
+        data: {
+            message: "Welcome to admin panel",
+            user: req.user
+        }
+    });
+});
+
 
 module.exports = router;
